f41cb5898f99a4a473cbe59fe64ea621f8ee9b1c
doudouhao <1361481896@qq.com>
2020-05-17 15:03:45 +0200

    update code

---
+++ ifelse.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+height = 1.75
+weight = 80.5
+bmi = weight / (height * height)
+# if bmi < 18.5:
+#     print("过轻")
+# elif   25 > bmi >= 18.5:
+#     print("正常")
+# elif  28 > bmi >= 25:
+#     print("过重")
+# elif  32 > bmi >= 28:
+#     print("肥胖")
+# else:
+#     print("严重肥胖")
+if 18.5 > bmi:
+    print("过轻")
+elif 18.5 <= bmi < 25:
+    print("正常")
+elif 28 > bmi >= 25:
+    print("过重")
+elif 32 > bmi >= 28:
+    print("肥胖")
+else:
+    print("严重肥胖")
